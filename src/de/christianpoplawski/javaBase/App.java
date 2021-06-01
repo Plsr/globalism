d37ce72dfbd7a00677e7806b94999f8fc28724d9
Plsr <christianpoplawski@gmail.com>
2021-06-01 22:06:57 +0200

    I haz window

---
+++ src/de/christianpoplawski/javaBase/App.java
@@ -0,0 +1,28 @@
+package de.christianpoplawski.javaBase;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+    public App() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+
+        setSize(250, 200);
+        setTitle("Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        EventQueue.invokeLater(() -> {
+            App ex = new App();
+            ex.setVisible(true);
+        });
+    }
+}
---
+++ src/de/christianpoplawski/javaBase/Board.java
@@ -0,0 +1,6 @@
+package de.christianpoplawski.javaBase;
+import javax.swing.JPanel;
+
+public class Board extends JPanel {
+    public Board() {}
+}
